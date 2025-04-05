7a66cb2558e2aeccd0889ba635fd44996ce592e4
haimnewclass <haim@newclass.co.il>
2025-04-06 01:26:24 +0300

    a

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
     <p class="read-the-docs">   
       Click on the Vite and TypeScript logos to learn more
-    </p>
+    </p>    
   </div>
 `
 
